89debd14f67b0ed83b41de71066c0ab791341a7b
ehj0529 <63188985+ehj0529@users.noreply.github.com>
2020-12-10 22:43:39 +0900

    2nd commit

---
+++ src/main/java/kr/co/fastcampus/cli/Member.java
@@ -0,0 +1,32 @@
+package kr.co.fastcampus.cli;
+
+import lombok.*;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@EqualsAndHashCode
+public class Member {
+    private int id ;
+    private String username ;
+    @NonNull  private String password ;
+
+
+    public Member( ResultSet resultSet){
+        try {
+            this.id = resultSet.getInt("id");
+            this.username = resultSet.getString("username");
+            this.password = resultSet.getString("password");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
